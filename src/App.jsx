eaee44be6451048b4d01d0b8078d3ac01731e26c
ed-matheus <edsonmatheus02@hotmail.com>
2026-04-22 23:44:04 -0300

    first commit: header component and starting the first body section

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+// Components
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      {/* Form */}
+      <section className='bg-white'>
+        <div>
+          <h2>Registrar leitura de hoje</h2>
+        </div>
+        <hr />
+        <form>
+
+        </form>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+// Icons
+import { FaBookBible } from "react-icons/fa6";
+import { FiMoon } from "react-icons/fi";
+
+const Header = () => {
+  return (
+    <div className='bg-white text-xl font-medium p-4 flex justify-between shadow-md mb-5'>
+      <div className="flex items-center gap-1.5">
+        <FaBookBible className="text-indigo-500" />
+        <h2 className="text-black">
+          Daily
+          <span className='text-indigo-500'>Word</span>
+        </h2>
+      </div>
+
+      <button className='border border-gray-200 rounded-md p-2'>
+        <FiMoon className="text-black" />
+      </button>
+    </div>
+  )
+}
+
+export default Header
